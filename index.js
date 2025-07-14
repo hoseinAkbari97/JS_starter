c82f6e336ddf18c7269b2d27caa6d475875e5a6f
Hossein <hossein16akbari@gmail.com>
2025-07-14 14:29:10 +0330

    2-7 Functions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // part 2: Basics
-//section 6: Arrays
-let selectedColors = ['red', 'blue'];
-selectedColors[2] = 'green';
-console.log(selectedColors.length);
+//section 7: functions
+function greet()    {
+    console.log('Hello Hossein');
+}
+
+greet();
