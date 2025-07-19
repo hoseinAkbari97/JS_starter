d1ec7d5abf4494f7abf8da057ab6c855a08dbcd9
Hossein <hossein16akbari@gmail.com>
2025-07-19 17:33:59 +0330

    #4-7For...in

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // part 4: Control Flow
-// section 1: Infinite Loops
+// section 1: For...in
+
+// Defining an object and its properties
+const person = {
+    name: 'Hossein',
+    age: 28
+};
+
+// Iterating through the person's properties
+for (let key in person)
+    console.log(key);
