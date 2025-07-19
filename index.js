06c1f01995a7e8964a5eacf8ffc5afae11da3fcb
Hossein <hossein16akbari@gmail.com>
2025-07-19 17:41:24 +0330

    #4-7-2-for...InArray

--- index.js
+++ index.js
@@ -2,11 +2,6 @@
 // section 1: For...in
 
 // Defining an object and its properties
-const person = {
-    name: 'Hossein',
-    age: 28
-};
-
-// Iterating through the person's properties
-for (let key in person)
-    console.log(key, person[key]);
+const colors = ['red', 'green', 'blue'];
+for (let index in colors)
+    console.log(index);
