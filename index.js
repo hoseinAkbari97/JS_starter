3a42eb7386e0bef5d41a1b16fd310416821147e4
Hossein <hossein16akbari@gmail.com>
2025-07-19 16:46:23 +0330

    #4-2 Switch case

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // part 4: Control Flow
-// section 1: if...else
-let hour = 10;
+// section 1: Switch...case
 
-if (hour >= 6 && hour < 12)
-    console.log('Good morning');
-else if (hour >= 12 && hour < 18)
-    console.log('Good afternoon');
-else 
-    console.log('Good evening');
+let role;
+
+switch (role) {
+    case 'guest':
+        console.log('Guest User');
+        break;
+    case 'moderator':
+        console.log('Moderator User');
+        break;
+    default:
+        console.log('Unknown User');
+}
