454250eb164225db60097d177c03447a9c53113f
Hossein <hossein16akbari@gmail.com>
2025-07-21 15:41:09 +0330

    4-12-Exercise3-FizzBuzz

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 // part 4: Control Flow
-// section 11: Exercise2
+// section 12: Exercise3-FizzBuzz
 
-console.log(isLandscape(800, 1600))
+//  Divisible by 3 ==>  Fizz
+//  Divisible by 5 ==>  Buzz
+// divisible by both 3 and 5 ==> FizzBuzz
+//  Not divisible by 3 or 5 ==>  input
+//  Not a number ==> 'Not a number'
 
-function isLandscape(width, height) {
-    return (width > height);
+const output = FizzBuzz(45)
+console.log(output)
+
+function FizzBuzz(input)    {
+    if (typeof input !== 'number')
+        return  'Not a number!';
+
+    if (input % 15 === 0)
+        return  'FizzBuzz';
+    
+    if (input % 3 === 0)
+        return 'Fizz';
+
+    if (input % 5 === 0)    
+        return 'Buzz';
+
+    return input
 }
