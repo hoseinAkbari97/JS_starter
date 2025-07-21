610a3b5117325b4614de4739268c56803999d4bb
Hossein <hossein16akbari@gmail.com>
2025-07-21 14:57:56 +0330

    #4-8-For...of

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // part 4: Control Flow
-// section 1: For...in
+// section 8: For...of
 
 // Defining an object and its properties
 const colors = ['red', 'green', 'blue'];
-for (let index in colors)
-    console.log(index, colors[index]);
+for (let color of colors)
+    console.log(color);
