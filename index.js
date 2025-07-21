0bbcfe531bf743d37ac1283c7ec9137f4477574d
Hossein <hossein16akbari@gmail.com>
2025-07-21 15:20:26 +0330

    4-10-1-Conditional operators

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 // part 4: Control Flow
 // section 10: Exercise
 
-function max_finder(num1, num2) {
-    if (num1 < num2)    {
-        console.log(num2);
-    }
-    else if (num1 > num2)   {
-        console.log(num1)
-    }
-    else
-        console.log("The numbers are equal.")
-}
+let number = max_finder(3,3);
+console.log(number);
 
-max_finder(3,3)
+function max_finder(num1, num2) {
+    return (num1 > num2) ? num1 : num2;
+}
