9caec2998803c45fe7a9dff852ed6e93a7c4620a
Hossein <hossein16akbari@gmail.com>
2025-07-22 16:24:47 +0330

    4-13-5-Demerit Points-Final refactor

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ function checkSpeed(speed)  {
     const speedLimit = 70;
     const kmPerPoint = 5;
 
-    if (speed < speedLimit + kmPerPoint)
-        console.log("Ok")
-    else {
-        const points = Math.floor((speed - speedLimit) / kmPerPoint);
-        if (points >= 12)
-            console.log("License suspended!!!");
-        else
-            console.log("points: ", points)
+    if (speed < speedLimit + kmPerPoint) {
+        console.log("Ok");
+        return;
     }
+        
+    
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12)
+        console.log("License suspended!!!");
+    else
+        console.log("points: ", points);
+    
 }
