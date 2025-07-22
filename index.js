d904255499fb8ebd630326692b7999073228a1b7
Hossein <hossein16akbari@gmail.com>
2025-07-22 15:51:42 +0330

    4-13-Demerit Points start

--- index.js
+++ index.js
@@ -1,27 +1,2 @@
 // part 4: Control Flow
-// section 12: Exercise3-FizzBuzz
-
-//  Divisible by 3 ==>  Fizz
-//  Divisible by 5 ==>  Buzz
-// divisible by both 3 and 5 ==> FizzBuzz
-//  Not divisible by 3 or 5 ==>  input
-//  Not a number ==> 'Not a number'
-
-const output = FizzBuzz(false)
-console.log(output)
-
-function FizzBuzz(input)    {
-    if (typeof input !== 'number')
-        return  NaN;
-
-    if (input % 15 === 0)
-        return  'FizzBuzz';
-    
-    if (input % 3 === 0)
-        return 'Fizz';
-
-    if (input % 5 === 0)    
-        return 'Buzz';
-
-    return input
-}
+// section 13: Exercise: Demerit Points
