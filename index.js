28acc1224cfa3805e619c34436c905fbe86ddde9
Hossein <hossein16akbari@gmail.com>
2025-07-22 16:07:42 +0330

    4-13-1-Initial solution

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 // part 4: Control Flow
 // section 13: Exercise: Demerit Points
+
+checkSpeed(445);
+
+function checkSpeed(speed)  {
+    if (speed < 75) 
+        console.log("ok")
+
+    else    {
+        let point = Math.floor((speed-70)/5) < 12;
+        if (point < 12)
+            console.log(point);
+        else
+            console.log('License suspended!!!');
+    }
+}
