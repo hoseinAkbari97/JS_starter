b386739bc392e3ded3e3cdcbff8c92a13cd809e7
Hossein <hossein16akbari@gmail.com>
2025-07-26 16:15:43 +0330

    4-15-1-testing the first implementation

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 // Write a function that takes an array and counts the number of truthy and falsy values in it
 // The falsy values are: false, null, undefined, 0, NaN, and ''
 
+const array = [0, null, undefined, '', NaN, false, 1, 'hello', true];
+console.log(countTruthy(array));
+
 function countTruthy(array) {
     let count = 0;
     for (let value of array) {
