b9815bebd5fdeb9df44f422540b444cc14ca613e
Hossein <hossein16akbari@gmail.com>
2025-07-28 15:59:31 +0330

    4-17-1-clean-code

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ console.log(sum(10));
 
 function sum(limit) {
   let answer = 0;
-  for (let i=0; i<=limit; i++) {
-    if (i%3===0 || i%5===0) {
+
+  for (let i = 0; i <= limit; i++) {
+    if (i % 3===0 || i % 5=== 0) {
       answer += i;
     }
   }
+  
   return answer;
 }
