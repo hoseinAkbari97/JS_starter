293665dd01fb388df21c8eebd315ea987674307b
Hossein <hossein16akbari@gmail.com>
2025-07-28 16:18:39 +0330

    4-18-1-Correct answer not with single responsibilty principle

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // part 4: Control Flow
 // section 18: Grade
 
-const marks = [80, 80, 50];
+const marks = [80, 90, 100];
 
-// console.log(calculateGrade(marks));
+console.log(calculateGrade(marks));
 
 function calculateGrade(marks) {
   let sum = 0;
@@ -11,5 +11,12 @@ function calculateGrade(marks) {
   for (let mark of marks)
     sum += mark;
 
-  let average = 
+  let average = sum / marks.length
+
+  if (average < 60) return 'F';
+  if (average < 70) return 'D';
+  if (average < 80) return 'C';
+  if (average < 90) return 'B';
+  return 'A';
+
 }
