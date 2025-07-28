a7597ea6ec6003f9d79d4eb548538acc1a16d964
Hossein <hossein16akbari@gmail.com>
2025-07-28 17:19:18 +0330

    4-20-first solution of the problem

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 // part 4: Control Flow
 // section 20: Prime Numbers
+
+showPrimes(10);
+
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++) {
+
+    let isPrime = true;
+    for (let factor = 2; factor < number; factor++) {
+      if (number % factor === 0)
+        isPrime = false;
+        break;
+    }
+    if (isPrime) console.log(number);
+  }
+}
