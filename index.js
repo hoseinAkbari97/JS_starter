432b361f0fb2051bed249bb95b8135bd972b56c7
Hossein <hossein16akbari@gmail.com>
2025-07-28 17:10:33 +0330

    4-20-start of the prime numbers

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 // part 4: Control Flow
-// section 19: Stars
-
-showStart(10)
-
-function showStart(rows) {
-  for (let row = 1; row <= rows; row++) {
-    let pattern = '';
-    for (i = 0; i < row; i++)
-      pattern += '*';
-    console.log(pattern)
-  }
-}
+// section 20: Prime Numbers
