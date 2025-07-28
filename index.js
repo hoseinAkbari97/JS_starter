f8dc99ad21124a1f2e6854a56000e86fbdd4a5e2
Hossein <hossein16akbari@gmail.com>
2025-07-28 16:40:19 +0330

    4-18-2-Refactoring the code for single responsibility principle

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 // part 4: Control Flow
 // section 18: Grade
 
-const marks = [80, 90, 100];
+const array = [80, 90, 100];
 
-console.log(calculateGrade(marks));
+console.log(calculateGrade(array));
 
 function calculateGrade(marks) {
-  let sum = 0;
-
-  for (let mark of marks)
-    sum += mark;
-
-  let average = sum / marks.length
-
+  const average = calculateAverage(marks);
+  
   if (average < 60) return 'F';
   if (average < 70) return 'D';
   if (average < 80) return 'C';
   if (average < 90) return 'B';
   return 'A';
 
+}
+
+function calculateAverage(array) {
+  let sum = 0;
+
+  for (let value of array)
+    sum += value;
+
+  return sum / array.length
 }
