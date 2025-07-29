356edd695fe71011138bd47898757e283d580843
Hossein <hossein16akbari@gmail.com>
2025-07-29 13:28:04 +0330

    5-0-Start of the objects

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-// part 4: Control Flow
-// section 20: Prime Numbers
-
-showPrimes(100);
-
-function showPrimes(limit) {
-  for (let number = 2; number <= limit; number++) 
-    if (isPrime(number)) console.log(number);
-}
-  
-  
-  function isPrime(number) {
-    for (let factor = 2; factor < number; factor++) 
-      if (number % factor === 0)
-        return false;
-
-    return true;
-  }
+// part 5: Objects
   
