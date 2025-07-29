45268621b60844bfee66ae262efdad8a9bcee85a
Hossein <hossein16akbari@gmail.com>
2025-07-29 13:48:01 +0330

    5-2-initializing factory functions

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 // part 5: Objects
-// part 1: Basics
+// part 2: Factory Functions
 
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  isVisible: true,
-  draw: function() {
-    console.log("Draw");
-  }
-};
 
-circle.draw();
