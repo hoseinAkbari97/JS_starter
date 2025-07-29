3322329bee4aad452f280ad8d06d2e9924ecc653
Hossein <hossein16akbari@gmail.com>
2025-07-29 14:00:02 +0330

    5-2-1-createCircle Factory Function

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // part 5: Objects
 // part 2: Factory Functions
 
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log('Draw')
+    }
+  };
+}
 
+circle1 = createCircle(2);
+console.log(circle1);
