fa4fbe82e4cd137ca629ec240c8033e599345b00
Hossein <hossein16akbari@gmail.com>
2025-08-05 15:39:38 +0330

    5-6-1-Using call and apply methods

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ function Circle(radius) {
   };
 }
 
-const Circle1 = new Function ('radius', `
-  this.radius = radius;
-  this.draw = function() {
-    console.log('draw');
-  }
-  `);
-
-const circle = new Circle1(1);
+Circle.call({}, 1);
+Circle.apply({}, [1]);
 
 const another = new Circle1(1);
